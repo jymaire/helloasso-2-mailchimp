da393c20077d487a162ad24be1547f792ceda06b
Jeremy MAIRE <jeremy.maire@zenika.com>
2022-06-21 17:10:24 +0200

    Fix codepostal et rajout logger

--- src/main/java/app/process/XlsxService.java
+++ src/main/java/app/process/XlsxService.java
@@ -5,16 +5,18 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
+
 @Service
 public class XlsxService {
-    Logger logger = Logger.getLogger("csv");
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     public List<XlsxModel> readXlsx(String path) {
         List<XlsxModel> xlsxData = new ArrayList<>();
@@ -22,7 +24,7 @@ public class XlsxService {
         File file = new File(path);
         String filePath = file.getAbsolutePath();
         File xlFile = new File(filePath);
-        if(!xlFile.exists() || xlFile.isDirectory()){
+        if (!xlFile.exists() || xlFile.isDirectory()) {
             JPopupMenu erreur = new JPopupMenu();
             erreur.add(new JMenuItem("Le fichier n'existe pas"));
             erreur.setVisible(true);
@@ -40,24 +42,37 @@ public class XlsxService {
                 } else {
                     XlsxModel currentLine = new XlsxModel();
                     Cell date = row.getCell(0);
-                    if(date !=null){
-                        currentLine.setDate(date.getLocalDateTimeCellValue());
-                        Cell email = row.getCell(1);
-                        currentLine.setEmail(email.getStringCellValue());
-                        Cell nom = row.getCell(2);
-                        currentLine.setNom(nom.getStringCellValue());
-                        Cell prenom = row.getCell(3);
-                        currentLine.setPrenom(prenom.getStringCellValue());
-                        Cell status = row.getCell(4);
-                        currentLine.setStatus(status.getStringCellValue());
-                        Cell tarif = row.getCell(5);
-                        currentLine.setTarif(tarif.getStringCellValue());
-                        Cell codePostal = row.getCell(8);
-                        currentLine.setCodePostal(Double.toString(codePostal.getNumericCellValue()));
-                        Cell entrepriseProjet = row.getCell(9);
-                        currentLine.setEntrepriseProjet(entrepriseProjet.getStringCellValue());
-                        xlsxData.add(currentLine);
-                        logger.info(currentLine.toString());
+                    if (date != null) {
+                        try {
+                            currentLine.setDate(date.getLocalDateTimeCellValue());
+                            Cell email = row.getCell(1);
+                            currentLine.setEmail(email.getStringCellValue());
+                            Cell nom = row.getCell(2);
+                            currentLine.setNom(nom.getStringCellValue());
+                            Cell prenom = row.getCell(3);
+                            currentLine.setPrenom(prenom.getStringCellValue());
+                            Cell status = row.getCell(4);
+                            currentLine.setStatus(status.getStringCellValue());
+                            Cell tarif = row.getCell(5);
+                            currentLine.setTarif(tarif.getStringCellValue());
+                            Cell codePostal = row.getCell(8);
+                            // Sometimes zip code is not set as a number by Excel...
+                            String codePostalValue;
+                            try {
+                                codePostalValue = Double.toString(codePostal.getNumericCellValue());
+                            } catch (IllegalStateException e) {
+                                codePostalValue = codePostal.getStringCellValue();
+                            }
+                            currentLine.setCodePostal(codePostalValue);
+
+                            Cell entrepriseProjet = row.getCell(9);
+
+                            currentLine.setEntrepriseProjet(entrepriseProjet.getStringCellValue());
+                            xlsxData.add(currentLine);
+                            LOGGER.info(currentLine.toString());
+                        } catch (Exception e) {
+                            LOGGER.error("Erreur pendant le traitement du fichier Excel : {}", e.getMessage());
+                        }
                     }
                 }
             }
